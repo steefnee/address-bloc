24cf7dd8fdf84b06ba5f0631d86e6451817474ba
Stephanie <stephaniepui@outlook.com>
2015-08-17 21:55:19 -0700

    Addignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,6 +17,18 @@ class AddressBook
             index +=1
             end
         @entries.insert(index, Entry.new(name, phone, email))
-        end
+    end
+    
+    def remove_entry(name, phone, email)
+        index = 0
+        @entries.each do |entry|
+        
+            if name == entry.name
+                break
+            end
+            index +=1
+            end
+        @entries.delete_at(index)
+    end
     
 end
