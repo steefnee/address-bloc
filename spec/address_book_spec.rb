9d235f57496fada528a054059446aadf757c1319
Stephanie <stephaniepui@outlook.com>
2015-08-19 01:18:58 -0700

    Assignment 21 completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -87,6 +87,33 @@ end
      end
 
 
+   end
+
+   context "Parse another CSV file" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eql 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Kowalski", "222-222-2222", "kowalski@penguins.com")
+    end
+
+    it "imports the 2nd entry" do
+     book.import_from_csv("entries_2.csv")
+     entry_two = book.entries[1]
+     check_entry(entry_two, "Rico", "333-333-3333", "rico@penguins.com")
+   end
+
+   it "imports the 3rd entry" do
+     book.import_from_csv("entries_2.csv")
+     entry_three = book.entries[2]
+     check_entry(entry_three, "Skipper", "111-111-1111", "skipper@penguins.com")
+   end
   end
+
 
 end
